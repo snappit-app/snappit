10b08bca24b9083c42bfcb2ff366d21820f94bc5
atiurin <pinkcolorrrs@gmail.com>
2025-08-24 14:05:02 +0700

    Add tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
 
 const host = process.env.TAURI_DEV_HOST;
 
 export default defineConfig(async () => ({
-  plugins: [solid()],
+  plugins: [solid(), tailwindcss()],
   clearScreen: false,
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     port: 1420,
     strictPort: true,
