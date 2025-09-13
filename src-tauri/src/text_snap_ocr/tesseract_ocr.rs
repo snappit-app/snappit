4bc8c6b9674e6f284d6a861b3ab3cd9ab37b60c0
atiurin <pinkcolorrrs@gmail.com>
2025-09-14 00:42:24 +0700

    Add dlls

--- src-tauri/src/text_snap_ocr/tesseract_ocr.rs
+++ src-tauri/src/text_snap_ocr/tesseract_ocr.rs
@@ -1,4 +1,5 @@
 use image::DynamicImage;
+use leptess::LepTess;
 
 use crate::text_snap_errors::TextSnapResult;
 
@@ -6,6 +7,10 @@ pub struct TextSnapTesseractOcr;
 
 impl TextSnapTesseractOcr {
     pub fn recognize(img: &DynamicImage) -> TextSnapResult<String> {
+        let mut lt = LepTess::new(None, "eng").unwrap();
+        lt.set_image_from_mem(rgb)?;
+        println!("{}", lt.get_utf8_text().unwrap());
+
         Ok(String::from("text"))
     }
 }
